c55b1978366d14ec9e8c92da2c66900695263929
Finble <elainefinn@hotmail.com>
2016-08-17 19:07:59 +0100

    Socket.io added to client (index.html) + server (server.js + code in js folder)

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
-var http = require('http').Server(app); // start a new server and use express app as boilerplate, anything express app listens to, the server should also listen to
+var http = require('http').Server(app); 
+var io = require('socket.io')(http); // socket expects to call http server
 
-app.use(express.static(__dirname + '/public'));  //__dirname = current dir
+app.use(express.static(__dirname + '/public'));  
+
+io.on('connection', function () { // can listen for events, pass name of event
+    console.log('User connected via socket.io!');
+});
 
 http.listen(PORT, function() {
    console.log('Server started!'); 
