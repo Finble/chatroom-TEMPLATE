5aac3632775a89a68dcee1f887500c9469178cc9
Finble <elainefinn@hotmail.com>
2016-08-19 09:51:37 +0100

    Styled chat.html page + tweaked socket.on(message) to separate message from messages

--- public/js/app.js
+++ public/js/app.js
@@ -14,15 +14,19 @@ socket.on('connect', function () {
     });
 });
 
+// update message to messages + add message var = list item, which goes inside unordered list
+
 socket.on('message', function (message) {
     var momentTimestamp = moment.utc(message.timestamp);  
-    var $message = jQuery('.messages');
+    var $messages = jQuery('.messages');  // change
+    var $message = jQuery('<li class="list-group-item"></li>');  // add
     
 	console.log('New message:');
 	console.log(message.text);
     
     $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
     $message.append('<p>' + message.text + '</p>');
+    $messages.append($message); // add
 });
 
 var $form = jQuery('#message-form');  
